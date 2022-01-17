b4db340529938195a28626a00b51a17ed3de3567
Ritesh <arora.ritesh.95@gmail.com>
2022-01-17 19:33:41 +0530

    products design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
